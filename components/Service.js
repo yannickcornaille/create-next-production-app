73db469b24bb6401bf7f07a3a1652452d0322b2b
Yannick Cornaille <yannick.cornaille@orange.com>
2023-12-06 16:23:59 +0100

    Event

--- components/Service.js
+++ components/Service.js
@@ -1,7 +1,12 @@
-const Service = () => (
-  <button>
-    <span>Ergonomie</span>
-    <span>5000</span>
-  </button>
-);
+const Service = () => {
+  const handleClick = () => {
+    console.log('CLICK !!!');
+  };
+  return (
+    <button onClick={handleClick}>
+      <span>Ergonomie</span>
+      <span>5000</span>
+    </button>
+  );
+};
 export default Service;
