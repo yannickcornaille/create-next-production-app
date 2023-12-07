cf7ce5318d69e21c916c5e67e3d8ba2ad566d331
Yannick Cornaille <yannick.cornaille@orange.com>
2023-12-07 11:45:42 +0100

    States

--- components/Service.js
+++ components/Service.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
-const Service = ({ name, price = 0 }) => {
-  const handleClick = () => {
-    console.log('CLICK !!!');
-  };
+const Service = ({ id, name, price = 0, selected = false, onServiceClick }) => {
   return (
-    <button onClick={handleClick}>
+    <button
+      onClick={() => onServiceClick(id)}
+      className={selected ? 'selected' : ''}
+    >
       <span>{name}</span>
       <span>{price}</span>
     </button>
@@ -14,7 +14,10 @@ const Service = ({ name, price = 0 }) => {
 
 Service.propTypes = {
   name: PropTypes.string.isRequired,
+  id: PropTypes.number,
+  onServiceClick: PropTypes.func,
   price: PropTypes.number,
+  selected: PropTypes.bool,
 };
 
 export default Service;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,49 @@
-// import styles from '../styles/Home.module.css';
+// import '../styles/Home.module.css';
+import { useState } from 'react';
+
 import Title from '@/components/Title';
 import Service from '@/components/Service';
 import servicesData from '@/assets/servicesData';
 
-const Home = () => (
-  <div className="home">
-    <Title />
-    {/* {servicesData.map((service) => (
+const Home = () => {
+  const [services, setServices] = useState(servicesData);
+  const handleServiceClick = (id) => {
+    setServices(
+      services.map((service) => {
+        if (service.id === id) {
+          return { ...service, selected: !service.selected };
+        }
+        return service;
+      })
+    );
+  };
+
+  const total = services.reduce((accumulator, service) => {
+    if (service.selected) {
+      return accumulator + service.price;
+    }
+    return accumulator;
+  }, 0);
+
+  return (
+    <div className="home">
+      <Title />
+      {/* {servicesData.map((service) => (
       <Service key={service.id} name={service.name} price={service.price} />
     ))} */}
-    {servicesData.map((service) => (
-      <Service key={service.id} {...service} />
-    ))}
-  </div>
-);
+      {services.map((service) => (
+        <Service
+          key={service.id}
+          {...service}
+          onServiceClick={handleServiceClick}
+        />
+      ))}
+      <button disabled>
+        <span>Total</span>
+        <span>{total}</span>
+      </button>
+    </div>
+  );
+};
 
 export default Home;
